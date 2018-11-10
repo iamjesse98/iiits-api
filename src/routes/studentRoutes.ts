1df8aa87163aaf4daecbc3ee7e2e300482ec03e7
Yadagalla Jaswanth Kumar <yadagallajaswanthkumar98@gmail.com>
2018-11-10 14:48:55 +0530

    Update studentRoutes.ts

--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { onlyPost } from '../controllers/controllerUtils';
 import { getToken, getData, query } from '../controllers/defaultControllers';
-import { studentIndex, educationHistory, parent, semCourseReg } from '../controllers/studentControllers';
+import { studentIndex, studentCreate, studentUpdate, educationHistory, parent, semCourseReg } from '../controllers/studentControllers';
 
 const router = new Router();
 
 router.get('/*', onlyPost)
       .post('/', studentIndex)
+      .post('/update', studentUpdate)
+      .post('/create', studentCreate)
       .post('/education_history', educationHistory)
       .post('/parent', parent)
       .post('/sem_course_reg', semCourseReg);
